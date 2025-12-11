99fcb108bd1c959f1b098784e3ab5b395a02f330
shambhuling69-jpg <shambhuling69@gmail.com>
2025-12-11 17:22:32 +0530

    Commited2

--- src/test/java/pac2/OrgTest.java
+++ src/test/java/pac2/OrgTest.java
@@ -8,6 +8,7 @@ public class OrgTest
 	public void createorgTest()
 	{
 		System.out.println("Execute Org TEst");
+		System.out.println("Execute Org TEst1");
 	}
     @Test
 	public void modifyorgTEst()
